8d36d84002320124a4ca4485ace561c414f63f44
Vicente Esteban <vicesteb@gmail.com>
2018-06-09 17:33:22 -0500

    Little reformat to app class

--- src/main/java/com/hojaalviento/store/StoreApplication.java
+++ src/main/java/com/hojaalviento/store/StoreApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoreApplication {
+public class StoreApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(StoreApplication.class, args);
 	}
 }
